9c533ee2b5d08bf7317f3d480f88fd2a75aa6edb
jorgecardvi <jorgecardvi@gmail.com>
2020-04-04 01:01:44 +0200

    primera funcion para mostrar el numero en la pantalla

--- js/app.js
+++ js/app.js
@@ -1,30 +1,42 @@
-function disminuirTecla(event){
-  var teclaId = event.currentTarget.id;
-  document.getElementById(teclaId).style.transform = "scale(0.95)";
-}
-
-function aumentarTecla(event){
-  var teclaId = event.currentTarget.id;
-  document.getElementById(teclaId).style.transform = "scale(1)";
-}
-
-//asignarEventosTeclas: function(){
-function asignarEventosTeclas(){
-  var teclas = document.getElementsByClassName('tecla');
-  for (var i = 0; i < teclas.length; i++) {
-    teclas[i].onmousedown = disminuirTecla; //this.disminuirTecla
-    teclas[i].onmouseup = aumentarTecla; //this.aumentarTecla
-  }
-}
-
-asignarEventosTeclas()
-/*
-Calculadora.init()
+//Objeto con toda la funcionalidad de la calculadora
 var Calculadora = {
   init: function(){
-    //aqui irian los llamados a las funciones que hacen cada cosa
-    //this.nombrefuncion()
+    //llamados a las funciones
+    this.asignarEventosTeclas()
   },
+  //funciones para el cambio de tamano en las teclas cuando se las pulsa y se las suelta
+  asignarEventosTeclas: function(){
+    var teclas = document.getElementsByClassName('tecla');
+    for (var i = 0; i < teclas.length; i++) {
+      teclas[i].onmousedown = this.disminuirTecla;
+      teclas[i].onmousedown = this.mostrarNumero;
+      //teclas[i].onmousedown = this.mouseDown;
+      teclas[i].onmouseup = this.aumentarTecla;
+    }
+  },
+  /*
+  mouseDown: function(){
+    this.disminuirTecla(event);
+    this.mostrarNumero(event);
+  },
+  */
+  disminuirTecla: function(event){
+    console.log('funcion disminuir');
+    var teclaId = event.currentTarget.id;
+    document.getElementById(teclaId).style.transform = "scale(0.95)";
+  },
+  aumentarTecla: function(event){
+    var teclaId = event.currentTarget.id;
+    document.getElementById(teclaId).style.transform = "scale(1)";
+  },
+  //funciones para que aperezcan los numeros en la pantalla
+  mostrarNumero: function(event){
+    console.log('funcion mostrar');
+    var teclaId = event.currentTarget.id;
+    var display = document.getElementById('display')
+    display.innerHTML = teclaId
+    console.log(typeof(teclaId));
 
+  }
 }
-*/
+Calculadora.init()
