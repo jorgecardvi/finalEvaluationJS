156a8ccdef47df84edaecae5b9c240c535841f93
jorgecardvi <jorgecardvi@gmail.com>
2020-04-07 00:46:13 +0200

    cambio en la logica de funciones

--- js/app.js
+++ js/app.js
@@ -9,8 +9,8 @@ var Calculadora = {
     var teclas = document.getElementsByClassName('tecla');
     for (var i = 0; i < teclas.length; i++) {
       teclas[i].onmousedown = this.disminuirTecla;
-      teclas[i].onclick = this.mostrarNumero;
       teclas[i].onmouseup = this.aumentarTecla;
+      teclas[i].onclick = this.leerTecla;
     }
   },
   disminuirTecla: function(event){
@@ -21,18 +21,41 @@ var Calculadora = {
     var teclaId = event.currentTarget.id;
     document.getElementById(teclaId).style.transform = "scale(1)";
   },
-  //funciones para que aperezcan los numeros en la pantalla
-  mostrarNumero: function(event){
-    console.log('funcion mostrar');
+  leerTecla: function(event){
     var teclaId = event.currentTarget.id;
-    var display = document.getElementById('display')
-    var numeroString = ""
     if (teclaId == "0" || teclaId == "1" || teclaId == "2" || teclaId == "3" || teclaId == "4" || teclaId == "5" || teclaId == "6" || teclaId == "7" || teclaId == "8" || teclaId == "9") {
-      this.numeroString = this.numeroString + teclaId
-      display.innerHTML = teclaId
-      console.log(typeof(teclaId))
-      console.log(this.numeroString);
+      //llamar a la funcion de Numero
+      this.numero = teclaId
+      Calculadora.construirNumero(this.numero)
+    }else if (teclaId == "on") {
+      //borra el numero
+    }else if (teclaId == "dividido") {
+      //funcion para dividir
+    }else if (teclaId == 'por') {
+      //funcion para multiplicar
+    }else if (teclaId == 'menos') {
+      //funcion restar
+    }else if (teclaId == 'mas') {
+      //funcion sumar
+    }else if (teclaId == 'igual') {
+      //resultado
+    }else if (teclaId == 'punto') {
+      //numero decimal
     }
-  }
+  },
+  construirNumero: function(numero){
+    this.nuevoNumero = this.nuevoNumero + numero
+    console.log("numero: " + this.nuevoNumero);
+    Calculadora.mostrarPantalla(this.nuevoNumero)
+  },
+  mostrarPantalla: function(resultado){
+    //esta funcion va a mostrar las cosas en la pantalla
+    var display = document.getElementById('display')
+    console.log('funcion para mostrar el primer numero');
+    display.innerHTML = resultado
+    }
+
+
+
 }
 Calculadora.init()
