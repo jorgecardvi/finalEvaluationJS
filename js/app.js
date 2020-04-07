8fc7a049d55fe3bbf371c4aa905020436f5e60d4
jorgecardvi <jorgecardvi@gmail.com>
2020-04-08 00:24:52 +0200

    funcionalidades de numero, falta limitar el display

--- js/app.js
+++ js/app.js
@@ -3,6 +3,7 @@ var Calculadora = {
   init: function(){
     //llamados a las funciones
     this.asignarEventosTeclas()
+    this.nuevoNumero=""
   },
   //funciones para el cambio de tamano en las teclas cuando se las pulsa y se las suelta
   asignarEventosTeclas: function(){
@@ -24,11 +25,14 @@ var Calculadora = {
   leerTecla: function(event){
     var teclaId = event.currentTarget.id;
     if (teclaId == "0" || teclaId == "1" || teclaId == "2" || teclaId == "3" || teclaId == "4" || teclaId == "5" || teclaId == "6" || teclaId == "7" || teclaId == "8" || teclaId == "9") {
-      //llamar a la funcion de Numero
-      this.numero = teclaId
-      Calculadora.construirNumero(this.numero)
+    //llamar a la funcion de Numero
+    this.numero = teclaId
+    Calculadora.construirNumero(this.numero)
     }else if (teclaId == "on") {
       //borra el numero
+      this.resultado = '0'
+      Calculadora.mostrarPantalla(this.resultado)
+      Calculadora.init()
     }else if (teclaId == "dividido") {
       //funcion para dividir
     }else if (teclaId == 'por') {
@@ -41,17 +45,62 @@ var Calculadora = {
       //resultado
     }else if (teclaId == 'punto') {
       //numero decimal
+      this.punto="."
+      Calculadora.construirNumero(this.punto)
+    }else if (teclaId == 'sign') {
+      //numero negativo
+      this.cambioSigno = "-"
+      Calculadora.construirNumero(this.cambioSigno)
     }
   },
   construirNumero: function(numero){
+    //condicion para chequear si hay punto
+    if (numero === "." && this.nuevoNumero.includes('.')) {
+      return
+    }
+    if (numero === "-" && this.nuevoNumero.includes('-')) {
+      this.nuevoNumero = this.nuevoNumero.slice(1)
+      console.log(this.nuevoNumero);
+      Calculadora.mostrarPantalla(this.nuevoNumero)
+      return
+    }
+    if (numero === "-") {
+      this.nuevoNumero = numero + this.nuevoNumero
+      Calculadora.mostrarPantalla(this.nuevoNumero)
+      return
+    }
+    if (this.nuevoNumero === "" && numero === "0") {
+      this.resultado = "0"
+      Calculadora.mostrarPantalla(this.resultado)
+      return
+    }
     this.nuevoNumero = this.nuevoNumero + numero
-    console.log("numero: " + this.nuevoNumero);
     Calculadora.mostrarPantalla(this.nuevoNumero)
   },
   mostrarPantalla: function(resultado){
     //esta funcion va a mostrar las cosas en la pantalla
     var display = document.getElementById('display')
-    console.log('funcion para mostrar el primer numero');
+    if(resultado.length === 0){
+      display.innerHTML = '0'
+      return
+    }
+    if (resultado.includes('.') && resultado.length >= 9) {
+      resultado = Number(resultado).toPrecision(8)
+      display.innerHTML = resultado
+      return
+    }else
+    if (resultado.includes('-') && resultado.length >= 9) {
+      console.log('signo y longitud');
+      resultado = Number(resultado).toPrecision(8)
+      display.innerHTML = resultado
+      return
+    }else
+    if (resultado.includes('.') && resultado.includes('-') && resultado.length >= 10) {
+      resultado = Number(resultado).toPrecision(8)
+      display.innerHTML = resultado
+      return
+    }
+    //resultado = Number(resultado).toPrecision(8)
     display.innerHTML = resultado
     }
 
@@ -59,3 +108,6 @@ var Calculadora = {
 
 }
 Calculadora.init()
+//https://www.google.com/search?client=firefox-b-d&q=build+a+calculator+in+js#kpvalbx=_b2KLXtvsOOLisAe6rIsI31
+//https://www.freecodecamp.org/news/how-to-build-an-html-calculator-app-from-scratch-using-javascript-4454b8714b98/
+//https://freshman.tech/calculator/
